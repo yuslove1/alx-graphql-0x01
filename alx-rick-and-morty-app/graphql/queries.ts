8c1097353cdf9d290fa47437673130d6376062e2
yuslove1 <adexyuslove@gmail.com>
2025-01-25 03:42:23 -0800

    chore: Configured GraphQl in our React App

--- alx-rick-and-morty-app/graphql/queries.ts
+++ alx-rick-and-morty-app/graphql/queries.ts
@@ -1,8 +1,11 @@
 import { gql } from "@apollo/client";
 
+//`gql` is a template literal tag that parses the query string into a query document.
+//filter: FilterEpisode is a custom input type that we will define later
+//The query will return the count, pages, next, and prev fields from the info object, and the id, name, air_date, and episode fields from the results object.
 export const GET_EPISODES = gql`
 query getEpisodes($oage: Int, $filter: FilterEpisode) {
-    episodes(page: $page, filter: $filter) {
+    episodes(page: $page, filter: $filter) { 
         info {
         count
         pages
